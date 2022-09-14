de232195e8ec2756b9e67df57d39bf830ad9e5d0
= <=>
2022-09-14 10:43:39 +0100

    add creation of cluster if not already there

--- install-renovate.sh
+++ install-renovate.sh
@@ -1,3 +1,10 @@
+# use kind to create a cluster, if it doesn't exist
+kind  get clusters | grep kind-renovate
+if [ $? -eq 1 ]
+then
+    kind create cluster --name kind-renovate
+fi
+
 # create renovate namespace, if it doesn't exist
 kubectl get ns renovate 2> /dev/null
 if [ $? -eq 1 ]
